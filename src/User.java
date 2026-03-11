4255e76000e4c3acafc2c030ce97904ce40c015a
Derek <derekgarcia2124@gmail.com>
2026-03-11 12:34:58 -0600

    comment

--- src/User.java
+++ src/User.java
@@ -1,8 +1,63 @@
 public class User {
+    String userName;
+    boolean canEdit;
+    void userMenu () {
+        //this will be overwritten
+    }
+    void back() {}
+
     //derek
 // customer base / parent
 // organizer extends customer
 // admin extends organizer
+
+
+} // user 
+
+class customer extends User {
+
+}
+
+class organizer extends User {
+
+}
+
+class admin extends User {
+
+    void add() {
+        // needs info as same as customer / organizer 
+        // has menu options
+
+    }
+    void view() {
+        // 2 options
+        // display users
+        // search
+    }
+
+    void displayUsers() {
+        // part of view 
+    }
+    void search() {
+        //part of view
+    }
+
+    void update() {
+        // 3 options
+        // change name
+        // change username
+        // change password
+        // if no user found message
+        // display menu options
+    
+    }
+    void delete() {
+        //delete member found
+        // error if no found
+        // display menu
+    }
+
+    
 
 
 }
