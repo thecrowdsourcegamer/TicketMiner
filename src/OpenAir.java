fa599ec59c5b5e0b645d9e6cf554ddd22c4b54ae
HaydeeRojoo <hirojoovalle@miners.utep.edu>
2026-03-12 23:12:32 -0600

    Update OpenAir.java

--- src/OpenAir.java
+++ src/OpenAir.java
@@ -1,3 +1,11 @@
-public class OpenAir {
+public class OpenAir extends Venue {
 
+    public OpenAir(int venueId, String name, String type, int capacity, double cost, String location) {
+        super(venueId, name, type, capacity, cost, location);
+    }
+
+    @Override
+    public String getVenueType() {
+        return "OpenAir";
+    }
 }
