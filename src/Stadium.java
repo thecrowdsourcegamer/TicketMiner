589cd4d5eca1ab1c45ff5b0f6cc43dd88f7a57c6
Derek <derekgarcia2124@gmail.com>
2026-03-13 18:05:35 -0600

    add stadium

--- src/Stadium.java
+++ src/Stadium.java
@@ -1,3 +1,11 @@
-public class Stadium {
+public class Stadium extends Venue {
 
+    public Stadium(int venueId, String name, String type, int capacity, double cost, String location) {
+        super(venueId, name, type, capacity, cost, location);
+    }
+
+    @Override
+    public String getVenueType() {
+        return "Stadium";
+    }
 }
